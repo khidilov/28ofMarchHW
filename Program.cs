bf9d01b7d9e9fe5052a43ce0a47c3d7bac548854
khidilov <100746123+khidilov@users.noreply.github.com>
2022-03-29 02:27:38 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using _28ofMarchHW.Models;
+using System;
+
+namespace _28ofMarchHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singer sn = new Singer("Alec", "Benjamin", 27);
+            Song sg = new Song("Shadow of Mine", "POP", "Alec Benjamin");
+            sg.AddRating(5);
+            sg.AddRating(6);
+            sg.AddRating(7);
+
+            Console.WriteLine(sg.GetAverageRating()); // averege must be 6 but program returns 5
+
+
+
+        }
+    }
+}
